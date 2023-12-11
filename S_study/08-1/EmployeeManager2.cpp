5ceae9c079707df36ed48c28b9584c5843a5121d
J-millar99 <wlwogus33@gmail.com>
2023-12-12 08:35:19 +0900

    new 08

--- S_study/08-1/EmployeeManager2.cpp
+++ S_study/08-1/EmployeeManager2.cpp
@@ -1,53 +1,69 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-class PermanentWorker
+class Employee
 {
     private:
         char name[100];
-        int salary;
-    
+
     public:
-        PermanentWorker(const char *name, int money) : salary(money)
+        Employee(const char *name)
         {
             strcpy(this->name, name);
         }
-        int GetPay() const
+        void ShowYourName() const
+        {
+            cout<<"name: "<<name<<endl;
+        }
+};
+
+class PermanentWorker : public Employee
+{
+    private:
+        int salary;
+    
+    public:
+        PermanentWorker(const char *name, int money) : Employee(name), salary(money)
+        { }
+        int Getpay() const
         {
             return salary;
         }
         void ShowSalaryInfo() const
         {
-            cout<<"name: "<<name<<endl;
-            cout<<"salary: "<<GetPay()<<endl<<endl;
+            ShowYourName();
+            cout<<"salary: "<<Getpay()<<endl<<endl;
         }
 };
 
 class EmployeeHandler
 {
     private:
-        PermanentWorker *empList[50];
+        Employee *empList[50];
         int empNum;
     
     public:
         EmployeeHandler() : empNum(0) {}
-        void AddEmployee(PermanentWorker *emp)
+        void AddEmployee(Employee *emp)
         {
             empList[empNum++] = emp;
         }
         void ShowAllSalaryInfo() const
         {
+            /*
             for (int i = 0; i < empNum; i++)
-            {
                 empList[i]->ShowSalaryInfo();
-            }
+            */
         }
         void ShowTotalSalary() const
         {
             int sum = 0;
+            /*
             for (int i = 0; i < empNum; i++)
                 sum += empList[i]->GetPay();
+            */
             cout<<"salary sum: "<<sum<<endl;
         }
         ~EmployeeHandler()
